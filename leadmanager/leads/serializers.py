6efb439e9ee9bb9e57efccb80ac7f6ab90a0ac38
unknown <sonwj0915@naver.com>
2021-03-11 17:06:44 +0900

    모델 관리법 및 url 설정 설명

--- leadmanager/leads/serializers.py
+++ leadmanager/leads/serializers.py
@@ -1,10 +1,14 @@
 from rest_framework import serializers
-from leads.models import Lead
+from leads.models import LeadModel
 
 # Lead Serializer
+
+# leads의 models.py에 LeadModels 클래스를 가져와 사용
+# model = (클래스 이름)
+# fields = 가져올 모델 범위
 
 
 class LeadSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Lead
+        model = LeadModel
         fields = '__all__'
